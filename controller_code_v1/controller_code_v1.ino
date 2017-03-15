78bdb1f1c06ed766602fb53a78e1f3c2a94fcc94
t4h3d1f <thoomasallen8@gmail.com>
2017-03-15 14:28:15 -0700

    add files

---
+++ controller_code_v1/controller_code_v1.ino
@@ -0,0 +1,85 @@
+/*
+   ETEC Capstone project
+   developed by Thoomas Allen
+   packet design
+   [status_byte/command_byte, pitch_byte, yaw_byte]
+*/
+const byte j_x = 2;
+const byte j_y = 1;
+const byte pwr_led = 3;
+const byte idle_led = 4;
+const byte run_led = 6;
+const byte err_led = 5;
+      char cmd[10];
+String incoming_msg = "";
+
+const int timeout = 1500;
+const int polling_rate = 100; //lower this number to increase the transmit rate/ make controls smoother
+
+unsigned long next_tx = millis();
+unsigned long last_rx = millis();
+
+boolean connection_lost = false;
+
+void setup() {
+  pinMode(j_x, INPUT);
+  pinMode(j_y, INPUT);
+  Serial.begin(9600);
+  digitalWrite(pwr_led, HIGH);
+  digitalWrite(idle_led, HIGH);
+  digitalWrite(run_led, LOW);
+  digitalWrite(err_led, LOW);
+}
+
+void loop() {
+  //timed loop, dont spam messages
+  while (millis() < next_tx) {
+    if (Serial.available() > 1)
+    {
+      delay(10);
+
+      int i = 0;
+      while (Serial.available() > 1)
+      {
+        cmd[i] = Serial.read();
+       // cmd[i++] = '\0';
+        i = i+1;
+      }
+      last_rx = millis();
+      connection_lost = false;
+    }
+
+  }
+  if(!connection_lost){
+      char b_2 = map(analogRead(j_x),0,1023,0,255);
+  char b_3 = map(analogRead(j_y),0,1023,0,255);
+  //Serial.write(0);
+  Serial.write(b_2);
+  Serial.write(b_3);}
+if(millis() > last_rx + timeout)
+{
+  cmd[0] = 'f';
+  connection_lost = true;
+}
+//Serial.write(analogRead(j_x));
+//Serial.write(analogRead(j_y));
+  if (cmd[0] == '1') //idle
+  {
+    digitalWrite(idle_led, HIGH);
+    digitalWrite(run_led, LOW);
+    digitalWrite(err_led, LOW);
+  }
+  if (cmd[0] == '2')//running
+  {
+    digitalWrite(idle_led, LOW);
+    digitalWrite(run_led, HIGH);
+    digitalWrite(err_led, LOW);
+  }
+  if (cmd[0] == 'f')//error
+  {
+    digitalWrite(idle_led, LOW);
+    digitalWrite(run_led, LOW);
+    digitalWrite(err_led, HIGH);
+  }
+  next_tx = millis() + polling_rate;
+}
---
+++ robot_code_v1/robot_code_v1.ino
@@ -0,0 +1,109 @@
+/*
+   ETEC Capstone project
+   developed by Thoomas Allen
+
+   packet design
+   [status_byte/command_byte, pitch_byte, yaw_byte]
+*/
+#define IDLE '1' //these are used for commands and reports between the robot and controller
+#define COMM_FAIL 'f'
+#define CODE_EXECUTE '2'
+#define PITCH 0x01
+#define YAW 0x02
+#define THROTTLE 0x03
+/*
+   min pitch 123
+   140 is centered
+   max pitch 160
+*/
+
+#include<Servo.h>
+
+Servo pitchServ, yawServ, esc;
+
+unsigned long cnt;
+long time_since_last_msg = 0;
+
+const int throttle_max = 1200;
+
+int j_x = 140;
+int j_y = 90;
+int throttle = 1100;
+String incoming_msg = "";
+
+boolean isRunning = false;
+
+void setup() {
+  pitchServ.attach(11);
+  yawServ.attach(10);
+  esc.attach(9);
+  Serial.begin(9600);
+  pitchServ.write(140);
+  yawServ.write(80);
+  esc.write(1000);
+}
+
+void loop() {
+  time_since_last_msg = millis() - cnt;
+
+  if (time_since_last_msg >= 1000)
+  {
+    Serial.write(IDLE);
+    cnt = millis(); 
+    j_x = 140;
+    j_y = 90;
+    throttle = 1000;
+  }
+  if (Serial.available() > 1)
+  {
+  //  Serial.print("recieving");
+    char cmd[10];
+    int i = 0;
+    delay(10);
+    while (Serial.available() > 0)
+    {
+      cmd[i] = Serial.read();
+      //cmd[i++] = '\0';
+      i = i+1;
+    }
+    incoming_msg = cmd;
+
+//Serial.write(cmd);
+//    char b_01 = incoming_msg & 0xF00;
+//    char b_02 = (incoming_msg << 8) & 0xF0;
+//    char b_03 = (incoming_msg << 16) & 0xF;
+
+    char b_01 = 0;
+    byte b_02 = cmd[0];
+    byte b_03 = cmd[1];
+    if (b_01 == 0)
+      Serial.print(CODE_EXECUTE);
+    if (b_03 == 0)
+      Serial.print(COMM_FAIL);
+          if (b_02 == 0)
+      Serial.print(COMM_FAIL);
+  
+  j_y = map(b_03, 0, 255, 80, 110);
+  j_x = map(b_02, 0, 255, 123, 157);
+  throttle = abs((b_02 - 128));
+  throttle = map (throttle, 0, 128, 1000, throttle_max);
+  cnt = millis();
+  }
+  pitchServ.write(j_x);
+  yawServ.write(j_y);
+  esc.write(throttle);
+
+  //pitchServ.write(125);
+  //yawServ.write(80);
+  //esc.write(1100);
+  //delay(1000);
+  //pitchServ.write(160);
+  //yawServ.write(100);
+  //esc.write(1200);
+  //delay(1000);
+  //pitchServ.write(145);
+  //yawServ.write(90);
+  //esc.write(1000);
+  //delay(1000);
+
+}
